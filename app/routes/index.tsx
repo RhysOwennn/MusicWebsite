edf75b6951f3fc7937e053cc2cb196716f942d3f
RhysOwennn <rowen@student.wethinkcode.co.za>
2022-06-12 20:50:25 +0200

    tailwind working

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div>
-      <h2 className="text-secondry text-4xl">Sybersyn</h2>
+      <h2 className="text-secondry text-4xl border-8">Sybersyn</h2>
     </div>
   );
 }
