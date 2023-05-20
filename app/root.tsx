63c32311fb1360691c2390352c9679398182bb9e
RhysOwennn <69479023+RhysOwennn@users.noreply.github.com>
2023-05-20 11:07:09 +0200

    fix netlify build

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import styles from "./styles/app.css";
+import styles from "./styles/tailwind.css";
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Sybersyn",
